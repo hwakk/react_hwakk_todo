9c2f39cbe42a333c3cd55afc67a40541f0c82d98
hwakk <x_xhwakk@naver.com>
2022-11-30 23:28:02 +0900

    feat: Components  추가

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
+
+function TodoList(props) {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
+
+export default TodoList;
