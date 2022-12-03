2492d20f4f677693f54cc3b2e72e0bc5c56012a4
hwakk <x_xhwakk@naver.com>
2022-12-04 00:00:01 +0900

    feat: Filter 추가 개발

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
@@ -7,10 +8,13 @@ function App() {
   // filter 전달하기
   // Todo 로컬스토리지에 저장하기
 
+  const filters = ["all", "completed", "active"];
+  const [filter, setFilter] = useState(filters[0]);
+
   return (
     <>
-      <Header />
-      <TodoList />
+      <Header filters={filters} filter={filter} />
+      <TodoList filter={filter} />
     </>
   );
 }
